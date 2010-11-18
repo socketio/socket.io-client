3b92d8b018e1c609f860dfa7be3fab6a237aa802
Saikat Chakrabarti <saikat@gomockingbird.com>
2010-11-17 18:19:16 -0800

    Do not override WEB_SOCKET_SWF_LOCATION if it already exists

--- socket.io.js
+++ socket.io.js
@@ -21,7 +21,8 @@ if ('jQuery' in this) jQuery.io = this.io;
 
 if (typeof window != 'undefined'){
   // WEB_SOCKET_SWF_LOCATION = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//cdn.socket.io/' + this.io.version + '/WebSocketMain.swf';
-  WEB_SOCKET_SWF_LOCATION = '/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf';
+  if (typeof WEB_SOCKET_SWF_LOCATION === 'undefined')
+    WEB_SOCKET_SWF_LOCATION = '/socket.io/lib/vendor/web-socket-js/WebSocketMain.swf';
 }
 
 /**
