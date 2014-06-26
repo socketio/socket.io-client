23e600ab0fb8d351d6b06dbcdca14ff290124666
Kevin Roark <ker2143@columbia.edu>
2014-06-25 18:54:18 -0700

    update to use engine.write callback

--- test/connection.js
+++ test/connection.js
@@ -53,20 +53,20 @@ describe('connection', function() {
 
   it('should not send volatile messages', function(done) {
     var socket = io({volatile: true, forceNew: true}); // forceNew because cache sockets not volatile
-    var recievedEvent = false;
+    var receivedEvent = false;
     socket.emit('volatile'); // this should be received
 
     socket.on('serverReceivedVolatile', function() {
       if (!receivedEvent) {
         // the message successfully sent the first time
         receivedEvent = true;
-        socket.io.engine.writable = false; // simulate disconnection
+        socket.io.numPacketsWriting++; // simulate writing stall in engine
         socket.emit('volatile'); // then emit another (shouldn't send)
       }
       throw new Error('this means the volatile event was received');
     });
     setTimeout(function() {
-      socket.io.writable = true;
+      socket.io.numPacketsWriting = 0;
       done();
     }, 500);
   });
