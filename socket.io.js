8d7d1487bbaa11b8cf720dea6dd888ce2aeabb8b
David Bau <david.bau@gmail.com>
2014-11-11 19:38:09 -0500

    Fix socket.io issue #1862
    See https://github.com/Automattic/socket.io/issues/1862 for the repro
    case.  Basically, if connecting to one namespace at the same time
    as disconnecting from another, the connection gets lost.  We should
    keep the connection alive as long as anybody is "connecting".

--- socket.io.js
+++ socket.io.js
@@ -141,7 +141,7 @@ function Manager(uri, opts){
   this.timeout(null == opts.timeout ? 20000 : opts.timeout);
   this.readyState = 'closed';
   this.uri = uri;
-  this.connected = [];
+  this.connecting = [];
   this.attempts = 0;
   this.encoding = false;
   this.packetBuffer = [];
@@ -391,12 +391,9 @@ Manager.prototype.socket = function(nsp){
   if (!socket) {
     socket = new Socket(this, nsp);
     this.nsps[nsp] = socket;
-    var self = this;
-    socket.on('connect', function(){
-      if (!~indexOf(self.connected, socket)) {
-        self.connected.push(socket);
-      }
-    });
+    if (!~indexOf(self.connecting, socket)) {
+      this.connecting.push(socket);
+    }
   }
   return socket;
 };
@@ -408,9 +405,9 @@ Manager.prototype.socket = function(nsp){
  */
 
 Manager.prototype.destroy = function(socket){
-  var index = indexOf(this.connected, socket);
-  if (~index) this.connected.splice(index, 1);
-  if (this.connected.length) return;
+  var index = indexOf(this.connecting, socket);
+  if (~index) this.connecting.splice(index, 1);
+  if (this.connecting.length) return;
 
   this.close();
 };
