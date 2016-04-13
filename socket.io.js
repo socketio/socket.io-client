27f60e1c1bb6eeed67a63832f8e6ef8ebb0c6b3f
Ben Budnevich <ben.bj32001@gmail.com>
2016-04-13 01:42:54 -0400

    Use test instead of match for consistency and performance

--- socket.io.js
+++ socket.io.js
@@ -2982,7 +2982,7 @@ var utf8 = _dereq_('utf8');
  * http://ghinda.net/jpeg-blob-ajax-android/
  */
 
-var isAndroid = navigator.userAgent.match(/Android/i);
+var isAndroid = /Android/i.test(navigator.userAgent);
 
 /**
  * Check if we are running in PhantomJS.
