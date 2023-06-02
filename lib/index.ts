3f93b2d74336d01a796f8b9f09c04ea910287b51
Ray Foss <git@vblip.com>
2023-06-01 22:30:31 -0500

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,8 @@ import { url } from "./url.js";
 import { Manager, ManagerOptions } from "./manager.js";
 import { Socket, SocketOptions } from "./socket.js";
 import * as debugModule from "debug"; // debug()
-const debug = (debugModule.default || debugModule)("socket.io-client") // esModuleInterop
+
+const debug = (debugModule.default || debugModule)("socket.io-client"); // esModuleInterop
 
 /**
  * Managers cache.
