d73094e3ca4ade0ffe075b58ba21ae845f9e5bfe
Ray Foss <git@vblip.com>
2023-06-06 01:38:10 -0500

    That line doesn't help
     the next line does.
    
    Debug shouldn't be destructured...

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import { url } from "./url.js";
 import { Manager, ManagerOptions } from "./manager.js";
 import { Socket, SocketOptions } from "./socket.js";
-import * as debugPkg from "debug"; // debug()
+import debugPkg from "debug"; // debug()
 
 const debug = (debugPkg.default || debugPkg)("socket.io-client"); // debug()
 
